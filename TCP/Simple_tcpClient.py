eb09f538cc425f61b220f7a3492a51e0de4229fc
Ítalo Martins <italo.mtr@gmail.com>
2021-09-06 20:25:00 -0300

    fix

--- TCP/Simple_tcpClient.py
+++ TCP/Simple_tcpClient.py
@@ -1,5 +1,5 @@
 from socket import *
-serverName = "192.168.0.143"
+serverName = "192.168.0.150"
 serverPort = 27123
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
